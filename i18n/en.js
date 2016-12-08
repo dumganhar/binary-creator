002fe64e08190aa380913102de68c9247fc532d2
James Chen <jianhua.chen@cocos2d-x.org>
2016-12-08 15:43:31 +0800

    Updates i18n

--- i18n/en.js
+++ i18n/en.js
@@ -1,4 +1,5 @@
 module.exports = {
+    convenient_tools: 'Convenient Tools',
     title: 'Rebuild prebuilt libaries',
     select_platform: 'Platform',
     waiting: 'Compiling, please wait ...',
--- i18n/zh.js
+++ i18n/zh.js
@@ -1,4 +1,5 @@
 module.exports = {
+    convenient_tools: '快捷工具',
     title: '生成预编译库',
     select_platform: '请选择平台',
     waiting: '正在编译, 请稍后...',
